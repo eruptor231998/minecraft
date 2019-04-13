1523ad6d4a20f81979c78fca900c393f0984e7a6
Reguig <eruptor231998@gmail.com>
2019-04-13 11:34:58 +0200

    Update Inventaire.java

--- Craftjava/src/Inventaire.java
+++ Craftjava/src/Inventaire.java
@@ -1,14 +1,13 @@
-
 public class Inventaire {
-	Objets[] inventaire = new Objets[64];
+	Objets[] inventaire = new Objets[73];
 	Main m = new Main();
-	
+
 	public Inventaire() {
-		for(int i =0; i<64 ; i++) {
-			this.inventaire[i] = new Objets("vide",1,false);
+		for(int i =0; i<73 ; i++) {
+			this.inventaire[i] = new Objets("vide",1,false,0);
 		}
 	}
-	
+
 	public void echange(int n) { //prend la pile entiere
 		Objets tampon = m.tenu;
 		m.tien(inventaire[n]);
