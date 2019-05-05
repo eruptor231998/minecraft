9e62f802843cfdfc8712819e10658bd653aacf99
Reguig <eruptor231998@gmail.com>
2019-05-05 22:49:51 +0200

    Update C1.java

--- Craftjava/src/C1.java
+++ Craftjava/src/C1.java
@@ -27,7 +27,7 @@ en dï¿½placer un dans la combinaison, on a un problï¿½me d'affichage pour 
 */
 public class C1 extends JPanel implements ActionListener,WindowListener,MouseListener {
 
-	static Image img=new ImageIcon("wallpaper.jpg").getImage();
+	static Image img=new ImageIcon("wallpaper.png").getImage();
 
 	static Image image2=new ImageIcon("wood1.png").getImage();
 	static Image image1=new ImageIcon("wood.png").getImage();
@@ -42,19 +42,23 @@ public class C1 extends JPanel implements ActionListener,WindowListener,MouseLis
 	static Image image13=new ImageIcon("wooden_slabs.png").getImage();
 	static Image image7=new ImageIcon("chest.png").getImage();
 	static Image image14=new ImageIcon("door.png").getImage();
-	static Image image10=new ImageIcon("Echelle.png").getImage();
+	static Image image10=new ImageIcon("Échelle.png").getImage();
 	static Image image11=new ImageIcon("fence.png").getImage();
 
-	static Image[] tableauImg = {null,image1,image2,image3,image4,image5,image6,image7,image8,image10,image11,image12,image13,image14};
+	static Image[] tableauImg = {null,image1,image2,image3,image4,image5,image6,image7,image8,image9,image10,image11,image12,image13,image14};
 	static Inventaire inv = new Inventaire();
 	static Liste_Objet<Integer, Objets> obj = new Liste_Objet<Integer, Objets>();
 
 	static int x=0;
 	static int y=0;
+	static int t=0;
+	static int ne=0;
+	static int boutonDroit=0;
+	static int boutonGauche=0;
 
     Integer[] matrice= {0,0,0,0,0,0,0,0,0};
 
-	ListeCraft<Integer[][], Objets > recette = new ListeCraft<Integer[][], Objets >(obj);
+	ListeCraft<String, Objets > recette = new ListeCraft<String, Objets >(obj);
 
 	public static void main(String args[]) {
 		C1 c = new C1();
@@ -74,43 +78,154 @@ public class C1 extends JPanel implements ActionListener,WindowListener,MouseLis
 		f.setTitle("Minecraft-master");
 		f.setVisible(true);
 		for (int i=1; i<tableauImg.length;i++) {
-		inv.inventaire[i+9] = (Objets) obj.get(i);}
+		inv.inventaire[i+9] = (Objets) obj.get(i);
+		inv.invnombre[i+9] = 1;}
 
 
 	}
 
 	public void paint(Graphics g) {
+
 		Color c = Color.GRAY;
 		Color d=Color.white;
 		// fond d'ï¿½cran
 		g.drawImage(img, 0,0,null);
 
-		//Bloc du rï¿½sultat
+		//Bloc du resultat et de la souris
 		g.setColor(c);
-		g.fillRect(650,150, 50, 50);
+		g.fillRect(650,150, 50, 50); // bloc resultat
+		g.fillRect(1100,10,50,50);//bloc souris
 		g.setColor(d);
 		g.drawRect(650,150, 50, 50);
+		g.drawRect(1100,10,50,50);
+
+
 		String rec="";
 		for (int t=0;t<matrice.length;t++) {
 			if (t<matrice.length-1) {
-				rec+= Integer.toString(matrice[t])+",";
+				rec+= Integer.toString(matrice[t]);
 
 			}
 			else {
 				rec+= Integer.toString(matrice[t]);
 			}
 		}
-		if (recette.containsKey(rec)) {
-		Objets resultat= recette.get(rec);
-		g.drawImage(tableauImg[resultat.image],650, 150,null);
+
+		//Bouton droit
+		if (boutonDroit==1) {
+			boutonDroit=0;
+		if ((x<550 && x>400)&&(100<y && y<250)) {
+	    	int i=(y/50);
+	    	int j=(x/50);
+
+	    	int n=((i*3)+j)-13;
+
+	    //On dessine quand on tient un objet
+	    if (t==0) {
+	    	t=1;
+	    	g.drawImage(tableauImg[inv.inventaire[n].image],1100, 10,null);
+	    }
+	    if (t==1) {
+	    	t=0;
+	    }
+	    inv.echange(n);
+	    }
+
+	    if ((x<1100 && x>50)&&(400<y && y<550)) {
+	    	int i=(y/50)-8;
+	    	int j=(x/50);
+
+	    	int n=((i*21)+j);
+
+
+	    if (t==0) {
+		    t=1;
+		    g.drawImage(tableauImg[inv.m.tenu.image],1100, 10,null);
+	    	g.drawString(String.valueOf(inv.m.nbobjtenu), 1100+40, 10+45);
+		}
+		if (t==1) {
+		    t=0;
+		}
+
+	    inv.echange(n+9);
+	    }
+		}
+
+
+		//Bouton gauche
+		if (boutonGauche==1 ){
+			boutonGauche=0;
+		if ((x<550 && x>400)&&(100<y && y<250)) {
+			int i=(y/50);
+			int j=(x/50);
+
+			int n=((i*3)+j)-13;
+
+			//On dessine quand on tient un objet
+			if (t==0) {
+		    	t=1;
+		    	g.drawImage(tableauImg[inv.m.tenu.image],1100, 10,null);
+		    	g.drawString(String.valueOf(inv.m.nbobjtenu), 1100+40, 10+45);
+			}
+		    if (t==1) {
+		    	t=0;
+		    }
+		inv.select_un(n);
+		}
+
+		if ((x<1100 && x>50)&&(400<y && y<550)) {
+			int i=(y/50)-8;
+			int j=(x/50);
+
+			 int n=((i*21)+j);
+
+			 if (t==0) {
+			    	t=1;
+			    	g.drawImage(tableauImg[inv.inventaire[n+9].image],1100, 10,null);
+			    }
+			    if (t==1) {
+			    	t=0;
+			    }
+
+		 inv.select_un(n+9);
+		 }
+		}
+	    //Je test le craft
+		if (ne==1) {
+			if (recette.containsKey(rec)) {
+				ne=0; // pour pas que le résultat s'affiche direct
+
+				for (int i=0;i<9;i++){
+
+					if (inv.inventaire[i].nom!="vide") {
+						int j=10;
+						while (inv.inventaire[j].nom!="vide") {
+							j++;
+
+						}
+
+						inv.echange_entre( i, j);
+					}
+
+
+				}
+
+				Objets resultat= recette.get(rec);
+				inv.dans_la_main(resultat);
+				g.drawImage(tableauImg[resultat.image],650, 150,null);
+
+				g.drawImage(tableauImg[inv.m.tenu.image],1100, 10,null);
+		    	g.drawString(String.valueOf(inv.m.nbobjtenu), 1100+40, 10+45);
+				}
+		}
+
+
+		//On réinitialise la matrice
 		for (int b=0;b<matrice.length;b++) {
 			if (matrice[b]!=0) {
 				matrice[b]=0;
 			}
 		}
-
-		}
-
 
 
 
@@ -119,7 +234,6 @@ public class C1 extends JPanel implements ActionListener,WindowListener,MouseLis
 			for (int j=0; j<3; j++) {
 				g.setColor(c);
 				g.fillRect(400+(j*50),100+(i*50), 50, 50);
-				//g.drawImage(image,400+(j*50) ,100+(i*50),null);
 				g.setColor(d);
 				g.drawRect(400+(j*50),100+(i*50), 50, 50);
 			}
@@ -136,6 +250,8 @@ public class C1 extends JPanel implements ActionListener,WindowListener,MouseLis
 		}
 		j+=1;
 		}
+
+
 		for (int n=0; n < inv.inventaire.length ; n++) {
 			if (inv.inventaire[n].plein==true) {
 				//dans l'inventaire
@@ -143,7 +259,7 @@ public class C1 extends JPanel implements ActionListener,WindowListener,MouseLis
 					int x =(((n-10)%21)*50);
 					int y =400+(50*((n-10)/21));
 					g.drawImage(tableauImg[inv.inventaire[n].image],50+x, y,null);
-					g.drawString(String.valueOf(inv.inventaire[n].nombre), 50+x+40, y+45);
+					g.drawString(String.valueOf(inv.invnombre[n]), 50+x+40, y+45);
 					}
 
 				//dans les combinaisons
@@ -151,7 +267,7 @@ public class C1 extends JPanel implements ActionListener,WindowListener,MouseLis
 					int x = 350+((n-1)%3)*50;
 					int y = 100+(50*((n-1)/3));
 					g.drawImage(tableauImg[inv.inventaire[n].image],50+x, y,null);
-					g.drawString(String.valueOf(inv.inventaire[n].nombre), 50+x+40, y+45);
+					g.drawString(String.valueOf(inv.invnombre[n]), 50+x+40, y+45);
 
 					int i=(y/50)-2;
 					int jj=(x/50)-7;
@@ -159,35 +275,12 @@ public class C1 extends JPanel implements ActionListener,WindowListener,MouseLis
 					int place=(i*3)+jj;
 
 					matrice[place]=inv.inventaire[n].image;
-
-
-					for (int k=0;k<matrice.length;k++) {
-
-							System.out.print(matrice[k]);
-					//repaint();
-
-
-
-
-
-					}
-
 				}
-
 			}
 		}
-
-
-
-
-
-
 	}
 
-	private String Str(int nombre) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+
 
 	@Override
 	public void windowActivated(WindowEvent arg0) {
@@ -235,76 +328,31 @@ public class C1 extends JPanel implements ActionListener,WindowListener,MouseLis
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
-
-
-
+		//On récupère le x et le y de la souris
+		x=e.getX();
+	    y=e.getY();
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
 		int buttonDown = e.getButton();
 
-	 	if (buttonDown == MouseEvent.BUTTON1) {
+	 	if (buttonDown == MouseEvent.BUTTON3) {
 	 		x=e.getX();
-			 y=e.getY();
-
-			 if ((x<550 && x>400)&&(100<y && y<250)) {
-				 int i=(y/50);
-				 int j=(x/50);
-
-				 int n=((i*3)+j)-13;
-
-				 System.out.println("Combinaison : "+n);
-
-			 inv.select_un(n);
-			 repaint();
-			 }
-
-			 if ((x<1100 && x>50)&&(400<y && y<550)) {
-				 int i=(y/50)-8;
-				 int j=(x/50);
-
-				 int n=((i*21)+j);
-
-				 System.out.println("Ressource : "+n);
+			y=e.getY();
+			boutonGauche=1;
+			repaint();
 
 
-			 inv.select_un(n+9);
-			 repaint();
-			 }
-
-	    } else if(buttonDown == MouseEvent.BUTTON3) {
-				x=e.getX();
-				 y=e.getY();
-
-				 if ((x<550 && x>400)&&(100<y && y<250)) {
-					 int i=(y/50);
-					 int j=(x/50);
-
-					 int n=((i*3)+j)-13;
-
-					 System.out.println("Combinaison : "+n);
-
-				 inv.echange(n);
-				 repaint();
-				 }
-
-				 if ((x<1100 && x>50)&&(400<y && y<550)) {
-					 int i=(y/50)-8;
-					 int j=(x/50);
-
-					 int n=((i*21)+j);
-
-					 System.out.println("Ressource : "+n);
-
-
-				 inv.echange(n+9);
-				 repaint();
-				 }
-
-		    }
+	    } else if(buttonDown == MouseEvent.BUTTON1) {
+			x=e.getX();
+			y=e.getY();
+			boutonDroit=1;
+			repaint();
+			}
 
 	}
+
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
@@ -327,15 +375,16 @@ public class C1 extends JPanel implements ActionListener,WindowListener,MouseLis
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		String com = e.getActionCommand(); //le bouton sélectionné
-		System.out.println("Action sur le composant : " + com);
 
 		if (com=="Craft") {
-		System.out.println("ok");
+		// je réinitialise x et y pour empêcher de changer la valeur de t
+		x=0;
+		y=0;
+		ne=1; //je dis au paint de tester le craft
 		repaint();}
 
 		if (com=="Reset") {
-		System.out.println("ok1");
-		}
+			    }
 
 	}
 
