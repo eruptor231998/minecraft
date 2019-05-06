9c72217f2908fa28cb4da43e39cb28e04099db92
Reguig <eruptor231998@gmail.com>
2019-05-06 10:59:02 +0200

    Create Objets.java

---
+++ Craftjava/src/dernier/src/Objets.java
@@ -0,0 +1,18 @@
+
+
+
+public class Objets {
+	String nom;
+	//int nombre;
+	boolean plein;
+	int image;
+	
+	public Objets( String i, boolean vrais, int image) {
+		this.nom=i;
+		//this.nombre = nombre;
+		this.plein = vrais;
+		this.image=image;
+		
+	}
+
+}
